4d6b5a3ed834fa859dbffdca76dbd6e271fc6cdc
karta1902613 <105021043@live.asia.edu.tw>
2016-11-14 16:08:32 +0800

    ex01 1114
    ex01 1114

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -3,11 +3,30 @@ package ex;
  * Date: 2016/11/14
  * Author: 1050210XX OOO
  */
+import java.util.*;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		Scanner scn=new Scanner(System.in);
+      		var(scn.nextInt());}
+	
+    public static float var(int n){
+    	Scanner scn=new Scanner(System.in);
+         int []data=new int[n];
+     for(int i=0;i<n;i++){
+         	int x =scn.nextInt();
+         	data[i]=x;}
+         int us =0;
+     	 int sum=0;
+     for(int i=0;i<n;i++){
+    	 us+=data[i];}
+	 float u=us/n;
+     for(int i=0;i<n;i++){
+    	 data[i]=(int) ((data[i]-u)*(data[i]-u));}
+     for(int i=0;i<n;i++){
+    	 sum+=data[i];}
+         float ans=0;
+     ans=(float)sum/n;
+     System.out.print((float)ans);
+         return ans;}}
